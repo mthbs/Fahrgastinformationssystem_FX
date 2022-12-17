810afb12fd55c9a727aaf7f5e97ef93954075891
mthbs <maxim.neumann@gmx.net>
2022-12-17 10:44:08 +0100

    Added Menu to the Window. Closes FIS-25

--- src/de/fis/Main.java
+++ src/de/fis/Main.java
@@ -4,12 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.stage.Stage;
+
 
 import java.sql.SQLException;
 
 
 public class Main extends Application {
+
+//    private MenuBar menuBar;
 
     public static void main(String[] args) throws SQLException {
         launch(args);
@@ -20,9 +26,21 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("MainBoard.fxml"));
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("style/root.css").toExternalForm());
+
+//        setMenuBar();
+
         stage.setScene(scene);
         stage.show();
 
     }
+
+//    private void setMenuBar() {
+//        menuBar = new MenuBar();
+//        Menu menu = new Menu("Bahnhofsanzeige");
+//        MenuItem menuItem1 = new MenuItem("Setze 12:00 Uhr");
+//        MenuItem menuItem2 = new MenuItem("Lade neu");
+//        menu.getItems().addAll(menuItem1,menuItem2);
+//        menuBar.getMenus().addAll(menu);
+//    }
 
 }
